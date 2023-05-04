0fb44b0e17481271bfc1f8f75ed2ebd9a579c437
wolfsoul <alejandrof011021@gmail.com>
2023-05-04 11:58:45 -0400

    Se creo la clase busqueda

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 import "colors";
-import { inquireMenu,pausa } from "./helpres/inquireMenu.js";
+import { inquireMenu, pausa } from "./helpres/inquireMenu.js";
+import Busqueda from "./models/Busqueda.js";
 
 const main = async () => {
   let opt;
+  const busqueda = new Busqueda();
 
   do {
     opt = await inquireMenu();
     console.log(opt);
+
+
     switch (opt) {
       case 1:
+        // mostrar mensaje 
         break;
 
       case 2:
         break;
     }
-    
-     if(opt !== 0) await pausa();
-    
+
+    if (opt !== 0) await pausa();
+
   } while (opt !== 0);
 };
 
---
+++ models/Busqueda.js
@@ -0,0 +1,25 @@
+
+
+class Busqueda{
+
+    historial=[];
+
+
+    constructor(){
+
+        // Leer DB si existe 
+    }
+
+    async buscarCiudad(lugar=''){
+
+        // peticion http
+
+        console.log(lugar);
+
+
+        return[]; // los lugares 
+    }
+
+}
+
+export default Busqueda
